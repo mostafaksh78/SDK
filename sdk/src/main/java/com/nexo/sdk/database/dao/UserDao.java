45e259a36e52e564a13eba6e4a7f22738bbe0740
mostafaksh78 <mosksh99@gmail.com>
2022-08-17 15:37:19 +0430

    Update UserDao.java

--- sdk/src/main/java/com/nexo/sdk/database/dao/UserDao.java
+++ sdk/src/main/java/com/nexo/sdk/database/dao/UserDao.java
@@ -6,7 +6,11 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
+import com.nexo.sdk.Global;
 import com.nexo.sdk.dataModels.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Dao
 public abstract class UserDao {
@@ -24,4 +28,24 @@ public abstract class UserDao {
     public abstract void deleteAll();
     @Query("DELETE FROM Users WHERE userID =:userID")
     public abstract void deleteByUserId(String userID);
+    public  User[] getRelatives(User user){
+        List<User> relatives = new ArrayList<>();
+        int rule = user.getRule();
+        switch (rule){
+            case 1:
+            case 0:{
+                List<User> commonCustomer = Global.database.getUserDao().getCommonCustomer(user.getCustomerID());
+                relatives.addAll(commonCustomer);
+                break;
+            }
+            case 3:
+            case 2:{
+                relatives.add(user);
+                break;
+            }
+        }
+        return relatives.toArray(new User[0]);
+    }
+    @Query("SELECT * FROM Users WHERE customerID =:customerID")
+    protected abstract List<User> getCommonCustomer(String customerID);
 }
